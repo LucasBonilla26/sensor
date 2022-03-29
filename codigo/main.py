9ffa563f3c7edfc9eee3b061277121e75640430e
LucasBonilla26 <lucasbonilla2000@gmail.com>
2022-03-30 00:52:54 +0200

    first integrationa

--- codigo/main.py
+++ codigo/main.py
@@ -1,4 +1,3 @@
-from asyncio import read
 from scipy import integrate
 import pandas as pd
 import numpy as np
@@ -14,22 +13,25 @@ def read_data():
     file = pd.read_csv('datos/datos.csv'); #May be a EXCEL
     return file
 
-def getPosition(packet_counter, acceleration):
-    
+def getIntegration(packet_counter, dataToIntegrate):
+    """
+    packet_counter is not needed i guess
+    """
     update_rate = 60
-    time = []
-
-    #Change for
-    #for i in packet_counter:
-        #time[] = i/60
-
-        #speed = integrate(acceleration,time) #(funcion, variable)
-        #print(speed)
-        #return speed
+    time_between_samples = 1/update_rate
+    dataIntegrated = [sum(dataToIntegrate[:i]) * time_between_samples for i in range(len(dataToIntegrate))]
+    return dataIntegrated
 
 
 file = read_data()
 #print(file)
-file.columns = ['1','2','3','4']
-print(file['1'])
-getPosition(file['1'],file['2'])
+file.columns = ['packetCounter','AccX','AccY','AccZ']
+print(file['AccX'])
+velocity_x = getIntegration(file['packetCounter'],file['AccX'])
+position = getIntegration(file['packetCounter'],velocity_x)
+print(position)
+
+
+# Rotation Matrix
+# cos(x) -sin(x)
+# sin(x) cos(x)
