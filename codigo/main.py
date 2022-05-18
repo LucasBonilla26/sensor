25b39cdf8db7a288e76c34451b30b69dc2c7079a
LucasBonilla26 <lucasbonilla2000@gmail.com>
2022-05-18 16:18:36 +0200

    new sprint script

--- codigo/main.py
+++ codigo/main.py
@@ -1,4 +1,6 @@
-#from scipy import integrate
+from scipy import signal
+from scipy import integrate
+from webbrowser import get
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,59 +13,88 @@ def read_data():
     """
     Este es el docstring de la función
     """
-    file = pd.read_csv('datos/acceleration.csv'); #May be a EXCEL
+    file = pd.read_csv('datos/pablodepie30_noparada.csv'); #May be a EXCEL
     return file
 
-def getIntegration(packet_counter, dataToIntegrate):
+def getIntegration(acceleration):
     """
     packet_counter is not needed i guess
     """
-    acceleration = [1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 2]
-    update_rate = 10 #60
+    update_rate = 100 #60
     time_between_samples = 1/update_rate
     dataIntegrated = [sum(acceleration[:i]) * time_between_samples for i in range(len(acceleration))]
     return dataIntegrated
 
-def getIntegrationV2():
-    acceleration = [1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 2, 0]
+def getIntegrationV2(vector):
+    vector[-1] = 0
     velocity = [0]
-    time = 0.1
+    time = 0.06
     for acc in acceleration:
         velocity.append(velocity[-1] + acc * time)
-    del velocity[0]
     
+    del velocity[0]
     return velocity
 
 def cumtrapz_example(x):
-    dtime = 0.1
+    dtime = 0.06
     dtVel = x * dtime;
     return np.cumsum([dtVel])
 
 file = read_data()
-print(file)
+#print(file)
 file.columns = ['packetCounter','AccX','AccY','AccZ']
-#print(file['Acc_X'].to_list())
-# print(file['AccX'])
-# # print(file['AccX'])
-# # velocity_x = getIntegration(file['packetCounter'],file['AccX'])
-# # position = getIntegration(file['packetCounter'],velocity_x)
+acceleration = np.array(file['AccX'])
+
+#plt.plot(acceleration)
+
+b,a = signal.butter(4,10/(100/2),'low',analog = False)
+y = signal.filtfilt(b,a, acceleration)
+y -= 9.8
+
+i=0
+while i < len(y):
+    if -0.6 < y[i] < 0.6:
+        y[i] = 0.0
+    i+=1
+
+plt.plot(y)
+# velocity = cumtrapz_example(y)
+# position = cumtrapz_example(velocity)
+velocity = integrate.cumtrapz(y,dx=1/60,initial=0);
+
+i=0
+while i < len(velocity):
+    if -0.3 < velocity[i] < 0.3:
+        velocity[i] = 0.0
+    i+=1
+
+position = integrate.cumtrapz(velocity,dx=1/60,initial=0);
+position *= 100
+# velocity = getIntegration(acceleration)
+# position = getIntegration(velocity)
+
+# velocity = getIntegrationV2(acceleration)
+# position = getIntegrationV2(velocity)
+
+#print(max(velocity))
+#print(min(velocity))
+
+#print(max(acceleration))
+#print(min(acceleration))
 # # print(position)
-# #print(getIntegrationV2())
-# #print(getIntegration(file['packetCounter'],file['AccX']))
-# #acceleration = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 5.0, 2.0, 0.0, -1.0, -3.5, -3.5, -3.5, -2.1, -1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0])
-acceleration = file['AccX']
-print(acceleration)
-velocity = cumtrapz_example(acceleration)
-position = cumtrapz_example(velocity)
-# result = cumtrapz_example(acceleration)
-plt.plot(acceleration)
-plt.plot(velocity)
-# plt.plot(velocity[74:106])
-plt.plot(position)
-# print(position[len(position)-1]-position[0])
+# print(position[65])
 # print(position[-1])
-plt.ylabel('some numbers')
+
+#plt.figure(1)
+#plt.plot(y)
+plt.plot(velocity)
+#plt.plot(y)
+# plt.figure(2)
+plt.plot(position)
+plt.ylabel('Sensor')
 plt.show()
+
+
 
 
 
