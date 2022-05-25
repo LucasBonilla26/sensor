93caa5010fde996903d82adf568bdc7d03b46a7a
Lucas <lubonilla@alumnos.unex.es>
2022-05-25 02:04:47 +0200

    localMaxss

--- sprint/Graphic.py
+++ sprint/Graphic.py
@@ -1,25 +1,30 @@
+from threading import local
 from scipy import signal
 from scipy import integrate
 from webbrowser import get
+from scipy.signal import argrelextrema
 import math
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 
 class Graphic:
     """Graphic Class"""
 
-    frequency = 0
-    __xAcceleration = []
-    __xAcceleration = []
-    res = []
-    data = []
-    
     def __init__(self):
         """Method's construsctor"""
-        print(" created")
+        print("created")
+        self.frequency = 1
+        self.derivative = []
+        self.res = []
+        self.data = []
+        self.time = []
+        self.localMaxs = []
+        self.localMins = []
+        self.indexMax = []
+        self.indexMin = []
     
-    def read_data():
+    def read_data(self):
         """
         Este es el docstring de la función
         """
@@ -35,5 +40,39 @@ class Graphic:
             return number
         stepper = 10.0 ** digits
         return math.trunc(stepper * number) / stepper
+
+    def show_local_max_min(self):
+        """
+        Show local maximums and minimums of the vector
+        """
+        #Minimums and local maximums 
+        localMaximums = argrelextrema(np.array(self.res), np.greater)
+        localMinimums = argrelextrema(np.array(self.res), np.less)
+        #Show minimums and local maximums
+        print(localMaximums)
+        print(len(localMaximums[0]))
+        i=0
+        while i < len(localMaximums[0]):
+            self.indexMax.append(localMaximums[0][i])
+            self.indexMin.append(localMinimums[0][i])
+            self.localMaxs.append(self.res[self.indexMax[i]])
+            self.localMins.append(self.res[self.indexMin[i]])
+            # print("res=" + str(self.res[indexMax]))
+            # plot.scatter(self.res[indexMax],self.res[indexMax], color='purple', s=15)
+            # plot.scatter(self.res[indexMin],self.res[indexMin], color='red', s=15)
+            i+=1
+        return 0
+
+    def derivative_cal(self):
+        """
+        Obtain the derivative of vector
+        """
+        i=1
+        while i < len(self.data)-1:
+            diffV = (self.data[i+1] - self.data[i-1])/2*self.frequency
+            self.derivative.append(diffV)
+            i+=1
+        return self.derivative
+
     
 
--- sprint/sprint.py
+++ sprint/sprint.py
@@ -1,6 +1,7 @@
 from ctypes import sizeof
 from scipy import signal
 from scipy import integrate
+from scipy.signal import find_peaks
 from webbrowser import get
 import pandas as pd
 import numpy as np
@@ -9,47 +10,9 @@ from scipy.signal import argrelextrema
 import math
 from Graphic import Graphic
 
-def derivative(amplitude,time,frequency):
-    i=1
-    diffLocalMaxim=[]
-    while i < len(amplitude)-1:
-        diffV = (amplitude[i+1] - amplitude[i-1])/2*frequency
-        plot.scatter(time[i],diffV, color='yellow', s=15)
-        diffLocalMaxim.append(diffV)
-        i+=1
-    return diffLocalMaxim
-
-def showLocalMaxMin(vector,time):
-    #Minimums and local maximums 
-    localMaximums = argrelextrema(np.array(vector), np.greater)
-    localMinimums = argrelextrema(np.array(vector), np.less)
-    #Show minimums and local maximums 
-    i=0
-    while i < len(amplitude[localMaximums]):
-        plot.scatter(time[localMaximums][i],amplitude[localMaximums][i], color='purple', s=15)
-        plot.scatter(time[localMinimums][i],amplitude[localMinimums][i], color='red', s=15)
-        i+=1
-
-# Get x values of the sine wave
-time = np.arange(0, 200, 0.2);
-# Amplitude of the sine wave is sine of a variable like time
-amplitude = np.sin(time)
-# print(amplitude)
-# Plot a sine wave using time and amplitude obtained for the sine wave
-plot.plot(time, amplitude, color="lightblue")
-# Give a title for the sine wave plot
-plot.title('Sine wave')
-# Give x axis label for the sine wave plot
-plot.xlabel('Time')
-# Give y axis label for the sine wave plot
-plot.ylabel('Amplitude = sin(time)')
-plot.grid(True, which='both')
-plot.axhline(y=0, color='k')
-# Display the sine wave
-
 graphic = Graphic()
 
-file = pd.read_excel('datos/brutos1.xlsx',sheet_name = 'Center of Mass'); #May be a EXCEL
+file = pd.read_excel('datos/brutos1.xlsx',sheet_name = 'Center of Mass') #May be a EXCEL
 
 print(file.columns)
 file.columns = ['Frame', 'CoM pos x', 'CoM pos y', 'CoM pos z', 'CoM vel x',
@@ -62,14 +25,49 @@ res_value = file['Res']
 print(len(x_value))
 
 i=0
+c=0
 while i < len(x_value):
     graphic.res.append(np.sqrt(x_value[i]**2+y_value[i]**2))
     if (graphic.res[i] - res_value[i]) != 0:
-        print("pene")
+        c+=1
     i+=1
-    
-#print(graphic.res)
 
+
+
+# Get x values of the sine wave
+graphic.time = np.arange(0, len(graphic.res), 1)
+# graphic.time = np.arange(0, 100, 0.1)
+# graphic.res = np.sin(graphic.time)
+# Plot a sine wave using time and amplitude obtained for the sine wave
+plot.plot(graphic.time, graphic.res, color="lightblue")
+# Give a title for the sine wave plot
+plot.title('Graphic res')
+# Give x axis label for the sine wave plot
+plot.xlabel('Time')
+# Give y axis label for the sine wave plot
+plot.ylabel('Res = sqrt(velx^2+vely^2)')
+plot.grid(True, which='both')
+plot.axhline(y=0, color='k')
+
+graphic.show_local_max_min()
+peaks, _ = find_peaks(graphic.res,height=0)
+print(peaks)
+i=0
+c=0
+while i<len(peaks):
+    if graphic.res[peaks[i]] != graphic.localMaxs[i]:
+        #print("distintos")
+        c+=1
+    i+=1
+print(c)
+#plot.plot(peaks,graphic.res[np.array(peaks)],"x")
+# i=0
+# print("for")
+# while i < len(graphic.localMaxs):
+#     plot.scatter(graphic.time[graphic.indexMax[i]],graphic.localMaxs[i], color='purple', s=15)
+#     i+=1
+
+plot.show()
 # while i < len():
 #     x = file['Com pos x'][i]
 #     y = file['Com pos y'][i]
