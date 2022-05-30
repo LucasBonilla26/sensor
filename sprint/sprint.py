3a5ee3e9853a3ec1e745b1b9ae41800ce99299ba
LucasBonilla26 <lucasbonilla2000@gmail.com>
2022-05-30 20:23:57 +0200

    filtro

--- sprint/sprint.py
+++ sprint/sprint.py
@@ -1,20 +1,58 @@
 from ctypes import sizeof
 from scipy import signal
 from scipy import integrate
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks,savgol_filter
 from webbrowser import get
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
 from scipy.signal import argrelextrema
-import math
+import math, heapq
+import scipy.interpolate as interpolate
 from Graphic import Graphic
+
+def derivative(amplitude,time,frequency):
+    i=1
+    diffLocalMaxim=[]
+    while i < len(amplitude)-1:
+        diffV = (amplitude[i+1] - amplitude[i-1])/2*frequency
+        plot.scatter(time[i],diffV, color='yellow', s=15)
+        diffLocalMaxim.append(diffV)
+        i+=1
+    return diffLocalMaxim
+
+def showLocalMaxMin(vector,time):
+    #Minimums and local maximums 
+    localMaximums = argrelextrema(np.array(vector), np.greater)
+    localMinimums = argrelextrema(np.array(vector), np.less)
+    #Show minimums and local maximums 
+    i=0
+    while i < len(vector[localMaximums]):
+        plot.scatter(time[localMaximums][i],vector[localMaximums][i], color='purple', s=15)
+        plot.scatter(time[localMinimums][i],vector[localMinimums][i], color='red', s=15)
+        i+=1
+
+# Get x values of the sine wave
+# time = np.arange(0, len(graphic.res), 1);
+# # Amplitude of the sine wave is sine of a variable like time
+# amplitude = np.sin(time)
+# # print(amplitude)
+# # Plot a sine wave using time and amplitude obtained for the sine wave
+# plot.plot(time, amplitude, color="lightblue")
+# # Give a title for the sine wave plot
+# plot.title('Sine wave')
+# # Give x axis label for the sine wave plot
+# plot.xlabel('Time')
+# # Give y axis label for the sine wave plot
+# plot.ylabel('Amplitude = sin(time)')
+# plot.grid(True, which='both')
+# plot.axhline(y=0, color='k')
+# Display the sine wave
 
 graphic = Graphic()
 
-file = pd.read_excel('datos/brutos1.xlsx',sheet_name = 'Center of Mass') #May be a EXCEL
+file = pd.read_excel('datos/brutos1.xlsx',sheet_name = 'Center of Mass'); #May be a EXCEL
 
-print(file.columns)
 file.columns = ['Frame', 'CoM pos x', 'CoM pos y', 'CoM pos z', 'CoM vel x',
        'CoM vel y', 'CoM vel z', 'CoM acc x', 'CoM acc y', 'CoM acc z',
        'Unnamed: 10', 'Res']
@@ -22,52 +60,55 @@ file.columns = ['Frame', 'CoM pos x', 'CoM pos y', 'CoM pos z', 'CoM vel x',
 x_value = file['CoM vel x']
 y_value = file['CoM vel y']
 res_value = file['Res']
+x_value = x_value[:int(len(x_value)/2)]
+y_value = y_value[:int(len(y_value)/2)]
+
+time = np.arange(0, len(y_value), 1);
 print(len(x_value))
 
 i=0
-c=0
 while i < len(x_value):
     graphic.res.append(np.sqrt(x_value[i]**2+y_value[i]**2))
-    if (graphic.res[i] - res_value[i]) != 0:
-        c+=1
     i+=1
 
+print(len(time))
+print(len(graphic.res))
+mean = np.mean(graphic.res)
+print("mean:" + str(mean))
 
+weight = 1/(graphic.res - mean)
 
-# Get x values of the sine wave
-graphic.time = np.arange(0, len(graphic.res), 1)
-# graphic.time = np.arange(0, 100, 0.1)
-# graphic.res = np.sin(graphic.time)
-# Plot a sine wave using time and amplitude obtained for the sine wave
-plot.plot(graphic.time, graphic.res, color="lightblue")
+# t,c,k = interpolate.splrep(time, graphic.res, k=3)
+# print(str(t) + str(c) + str(k))
+# spline = interpolate.BSpline(t, c, k, extrapolate=True)
+
+# print(max(graphic.res))
+# max_value = max(graphic.res)
+# max_value_index = graphic.res.index(max_value)
+# print("Max value:" + str(max_value) + "Max value index:" + str(max_value_index))
+
+# global_maximums = []
+# max(x_value)
+# i=0
+# while i < len(x_value)-300:
+#     max_value_index = graphic.res.index(max(graphic.res))
+#     global_maximums.append(max(graphic.res))
+#     i=+300
+
+# print("global maximus" + str(global_maximums))
+
+plot.plot(time, graphic.res, color="lightblue")
 # Give a title for the sine wave plot
-plot.title('Graphic res')
-# Give x axis label for the sine wave plot
+plot.title('Sine wave')
+# # Give x axis label for the sine wave plot
 plot.xlabel('Time')
-# Give y axis label for the sine wave plot
-plot.ylabel('Res = sqrt(velx^2+vely^2)')
+# # Give y axis label for the sine wave plot
+plot.ylabel('Amplitude = sin(time)')
 plot.grid(True, which='both')
 plot.axhline(y=0, color='k')
 
-graphic.show_local_max_min()
-peaks, _ = find_peaks(graphic.res,height=0)
-print(peaks)
-i=0
-c=0
-while i<len(peaks):
-    if graphic.res[peaks[i]] != graphic.localMaxs[i]:
-        #print("distintos")
-        c+=1
-    i+=1
-print(c)
-#plot.plot(peaks,graphic.res[np.array(peaks)],"x")
-# i=0
-# print("for")
-# while i < len(graphic.localMaxs):
-#     plot.scatter(graphic.time[graphic.indexMax[i]],graphic.localMaxs[i], color='purple', s=15)
-#     i+=1
+#print(graphic.res)
 
-plot.show()
 # while i < len():
 #     x = file['Com pos x'][i]
 #     y = file['Com pos y'][i]
@@ -102,4 +143,4 @@ plot.show()
 #     plot.scatter(time[localMinimumsDiff[i]],diffLocalMaxim[localMinimumsDiff[i]], color='black', s=15)
 #     i+=1
     
-# plot.show()
+plot.show()
