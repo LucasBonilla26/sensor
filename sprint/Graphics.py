a1152275b3c4b087b605710af3cb10fa58902be3
LucasBonilla26 <lucasbonilla2000@gmail.com>
2022-06-16 19:34:50 +0200

    check time value

--- sprint/Graphics.py
+++ sprint/Graphics.py
@@ -6,6 +6,7 @@ import cubic_spline
 import xlsxwriter
 from scipy import signal
 from scipy.signal import find_peaks,savgol_filter
+import datetime as dt
 
 class Graphics():
 
@@ -21,20 +22,24 @@ class Graphics():
         self.segment4 = []
         self.segment5 = []
         self.segment6 = []
+        self.first_sheet = "General Information"
+        self.start_time = 0
 
         self.frame = []
-        #r'L:\.shortcut-targets-by-id\1Hs9L2qhd3LpjsVWp9cF_HjklAV_TVHBE\02_INVESTIGACIONES\Futbol\Territorio Gaming-Sta Teresa\04_Registros\Base\Loba.xlsx'
-        self.excel_file = r'L:\.shortcut-targets-by-id\1Hs9L2qhd3LpjsVWp9cF_HjklAV_TVHBE\02_INVESTIGACIONES\Futbol\Territorio Gaming-Sta Teresa\04_Registros\Base\MJ.xlsx'
+        #r'L:\.shortcut-targets-by-id\1Hs9L2qhd3LpjsVWp9cF_HjklAV_TVHBE\02_INVESTIGACIONES\Futbol\Territorio Gaming-Sta Teresa\04_Registros\Base\Sara.xlsx'
+        self.excel_file = r'L:\.shortcut-targets-by-id\1Hs9L2qhd3LpjsVWp9cF_HjklAV_TVHBE\02_INVESTIGACIONES\Futbol\Territorio Gaming-Sta Teresa\04_Registros\Base\Sara.xlsx'
 
         
     def load_file(self, file_path, sheet_name, n_values=-1):
         print("Reading file...")
         
         file = pd.read_excel(file_path, sheet_name) #Excel
+        
+        self.take_start_time(file_path)
 
         file.columns = ['Frame', 'CoM pos x', 'CoM pos y', 'CoM pos z', 'CoM vel x',
-            'CoM vel y', 'CoM vel z', 'CoM acc x', 'CoM acc y', 'CoM acc z',
-            'Unnamed: 10', 'Res']
+            'CoM vel y', 'CoM vel z', 'CoM acc x', 'CoM acc y', 'CoM acc z']
+
 
         print("Loading columns...")
         x_value = file['CoM vel x'][:n_values]
@@ -44,6 +49,13 @@ class Graphics():
         print("Calculating speed...")
         self.data = [np.sqrt(x**2 + y**2) for x,y in list(zip(x_value, y_value))]
         # return self.data
+
+    def take_start_time(self,file_path):
+        generalsheet = pd.read_excel(file_path, self.first_sheet)
+        self.start_time = generalsheet.iat[2,1]
+        print(self.start_time)
+        print(type(self.start_time.hour))
+        self.start_time = dt.time(self.start_time.hour,self.start_time.minute,self.start_time.second)
 
     def umbralize(self, factor=0):
         threshhold = np.mean(self.data)
@@ -80,7 +92,7 @@ class Graphics():
         workbook = xlsxwriter.Workbook(self.excel_file)
         umbral = 0.31
         #máximos [2733,14822,37474,43953,51529,55242,64208,69019,81255,85009,105422,135371,140114,144357,147569]
-        max = [2733,14822,37474,43953,51529,55242,64208,69019,81255,85009,105422,135371,140114,144357,147569]
+        max = [12479,27288,48830,52003,54013,59022,62181,85891,86633,92485,92617,110480,114988,140829]
         
         for x in max:
             aux = []
@@ -99,7 +111,7 @@ class Graphics():
             frame_aux = np.flip(frame_aux)
 
             i = x
-            #i+=1 
+            i+=1 
             while i < len(self.data):
                 if self.data[i] > umbral:
                     self.segment1 = np.append(self.segment1,self.data[i])
@@ -115,8 +127,13 @@ class Graphics():
             worksheet = workbook.add_worksheet()
             
             for x in range(len(self.segment1)):
-                worksheet.write(row,1,self.segment1[x])
+                worksheet.write(row,2,self.segment1[x])
                 worksheet.write(row,0,frame_aux[x])
+                seconds = dt.timedelta(seconds=frame_aux[x]/240/24/60/60*10000)
+                frame_time = (dt.datetime.combine(dt.date(1,1,1),self.start_time) + seconds).time()
+                print(frame_time)
+                print(frame_time.strftime("%H:%M:%S"))
+                worksheet.write(row,1,frame_time.strftime("%H:%M:%S.%f"))
                 row+=1
 
         workbook.close()
@@ -127,7 +144,7 @@ if __name__ == "__main__":
     graphic = Graphics()
     #graphic.load_file("datos/brutos1.xlsx","Center of Mass") No poner nada para coger la gráfica entera
     #El último parámetro espécifica hasta que frame se quiere cargar de los datos, si se quiere cargar la gráfica entera quitar el valor
-    graphic.load_file("datos/brutos1.xlsx","Center of Mass",170000) 
+    graphic.load_file(r"C:\Users\BioEr\Desktop\FutbolBase\Sara_SportExtremadura.xlsx","Center of Mass",150000) 
     #graphic.umbralize(2.0)
     #graphic.cubic_spline_smooth()
     #graphic.low_filter()
